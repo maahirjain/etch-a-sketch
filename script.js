c141b16065cc7352d8d2b32ed61560669687f83c
Maahir Jain <maahirdjain@gmail.com>
2024-06-17 14:11:19 +0530

    Add initial CSS styling to page

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ for (let i = 0; i < gridSide; i++) {
 
     for (let i = 0; i < gridSide; i++) {
         let div = document.createElement("div");
-        div.style.width = "16px";
-        div.style.height = "16px";
+        div.style.width = "32px";
+        div.style.height = "32px";
         div.style.border = "1px solid black";
         divContainer.appendChild(div);
     }
