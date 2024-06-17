6855b64e0cdc5e632822c1f20f2f2805aa75c57b
Maahir Jain <maahirdjain@gmail.com>
2024-06-17 15:37:07 +0530

    Add grid JS

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
-let gridSide = 16;
+let input = document.querySelector("input");
+let checkbox = document.querySelector("#grid-lines");
 
-for (let i = 0; i < gridSide; i++) {
-    let divContainer = document.createElement("div");
+function createGrid(gridSide) {
+    document.querySelector(".grid").remove();
+
+    let grid = document.createElement("div");
+    grid.classList.add("grid");
+    grid.style.border = "1px solid black";
+    document.querySelector(".sketch-area").appendChild(grid);
 
     for (let i = 0; i < gridSide; i++) {
-        let div = document.createElement("div");
-        div.style.width = "32px";
-        div.style.height = "32px";
-        div.style.border = "1px solid black";
-        divContainer.appendChild(div);
+        let divContainer = document.createElement("div");
+    
+        for (let i = 0; i < gridSide; i++) {
+            let div = document.createElement("div");
+            let side = (546 - 2 * gridSide)/gridSide;
+            div.style.width = `${side}px`;
+            div.style.height = `${side}px`;
+            div.style.border = "1px solid black";
+            divContainer.appendChild(div);
+        }
+    
+        grid.appendChild(divContainer);
     }
-
-    document.querySelector(".sketch-area").appendChild(divContainer);
 }
 
+createGrid(16);
+
+input.addEventListener("input", () => {
+    document.querySelector("p").textContent = `${input.value} x ${input.value}`;
+    createGrid(input.value);
+});
+
+checkbox.addEventListener("change", () => {
+    let gridChildren = Array.from(document.querySelector(".grid").getElementsByTagName('*'));
+    if (checkbox.checked) {
+        document.querySelector(".grid").style.border = "1px solid black";
+        gridChildren.forEach(div => { return div.style.borderColor = "black"; });
+    } else {
+        document.querySelector(".grid").style.border = "2px solid black";
+        gridChildren.forEach(div => { return div.style.borderColor = "white"; });
+    }
+});
